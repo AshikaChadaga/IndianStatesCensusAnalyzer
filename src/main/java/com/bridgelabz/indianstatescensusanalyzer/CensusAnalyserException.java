15468c0b58d2e1fe00c8718f150c8c0dcce363eb
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-21 17:01:13 +0530

    REFACTORED: Custom Exception to handle Incorrect Header and Delimiter

--- src/main/java/com/bridgelabz/indianstatescensusanalyzer/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/indianstatescensusanalyzer/CensusAnalyserException.java
@@ -3,7 +3,7 @@ package com.bridgelabz.indianstatescensusanalyzer;
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM
+        CENSUS_FILE_PROBLEM, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER
     }
 
     ExceptionType type;
